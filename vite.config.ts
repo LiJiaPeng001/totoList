6e88b7cd69d1ce29732829a77840e10153d7ddaf
lijiapeng001 <1146417576@qq.com>
2023-06-10 12:00:37 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [Unocss(), react()],
 })
