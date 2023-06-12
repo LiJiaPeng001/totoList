6c0cf9bca43c76baae0199d0a6723d253b4a6634
lijiapeng001 <1146417576@qq.com>
2023-06-12 12:15:42 +0800

    feat: add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-// import { useState } from 'react'
+import { Button, DatePicker } from 'antd'
 
 function App() {
   return (
     <>
+      <Button>huhu</Button>
+      <DatePicker></DatePicker>
       <h1 className="h-100vh w-full center-flex text-20">Hello,world</h1>
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
 import App from './App.tsx'
 import 'uno.css'
 import '@peeeng/css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <App />
+      </ConfigProvider>
   </React.StrictMode>,
 )
