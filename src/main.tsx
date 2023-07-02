ca44d358f430667b80da868d54abf7aa6fb4ebbe
lijiapeng001 <1146417576@qq.com>
2023-07-02 10:23:27 +0800

    feat: add react-redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import { RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import routes from './router/index'
+import store from './store/app'
+
 import 'uno.css'
 import '@peeeng/css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ConfigProvider locale={zhCN}>
-    <RouterProvider router={routes} />
-      </ConfigProvider>
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+       </Provider>
+    </ConfigProvider>
   </React.StrictMode>,
 )
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,14 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { Button } from 'antd'
+import { increment } from '~/store/count'
+import type { AppState } from '~/store/app'
+
 function Index() {
+  const count = useSelector((state: AppState) => state.count.value)
+  const dispatch = useDispatch()
   return <>
-    <div>index</div>
+    <Button onClick={() => dispatch(increment())}>Add</Button>
+    <div>{count}</div>
   </>
 }
 
