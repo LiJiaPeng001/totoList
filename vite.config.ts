b82f799447b5c6531d87ea835bcd4193108d0305
lijiapeng001 <1146417576@qq.com>
2023-07-04 12:01:50 +0800

    chore: add unimport react & react-router

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
+import Unimport from 'unimport/unplugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,20 @@ export default defineConfig({
       '~': `${path.resolve(__dirname, './src')}`,
     },
   },
-  plugins: [Unocss(), react()],
+  plugins: [
+    Unocss(),
+    react(),
+    Unimport.vite({
+      dts: true,
+      dirs: [
+        './composables/*',
+      ],
+      presets: [
+        'react',
+        'react-router',
+      ],
+    }),
+  ],
   server: {
     port: 2333,
     host: true,
