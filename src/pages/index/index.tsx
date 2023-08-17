0288c4b7717712bd64c3c862cdf0631fd5aaff74
lijiapeng001 <1146417576@qq.com>
2023-08-17 20:24:47 +0800

    feat: add

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -6,9 +6,15 @@ import type { AppState } from '~/store/app'
 function Index() {
   const count = useSelector((state: AppState) => state.count.value)
   const dispatch = useDispatch()
+  const [name, setName] = useState('wuhu')
+  function changeName() {
+    setName('IUIUIU')
+  }
   return <>
     <Button onClick={() => dispatch(increment())}>Add</Button>
     <div>{count}</div>
+    <div>{name}</div>
+    <Button onClick={() => changeName()}>ChangeName</Button>
   </>
 }
 
